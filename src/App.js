a95ae940de9c96b92c807a76999ad62cd9595f80
Salis zar <saliszar@Saliss-iMac.local>
2019-10-03 11:54:36 +0500

    [add]deploy ivol

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 import Login from './scenes/Login/Login';
 import Signin from './scenes/Signin/Signin';
+import ReactDOM from 'react-dom';
+import {BrowserRouter, Link, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <Login/>
-      <Signin/>
-    </div>
-  );
+export default class App extends Component {
+  render(){
+    return (
+      <div className="App">
+    
+        <BrowserRouter>
+          <div>
+          <ul>
+            <li>
+              <Link to="/">Login</Link>
+            </li>
+            <li>
+            <Link to="/signin">Signin</Link>
+            </li>
+            
+          </ul>
+          <Route exact path="/" component={Login} />
+          <Route path="/signin" component={Signin} />
+          </div>
+        </BrowserRouter>
+        
+      </div>
+    );
+  }
+  
 }
 
-export default App;
+
