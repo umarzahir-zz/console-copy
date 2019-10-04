9b4a59b03347499f4323fd90d8cd63e352dbe3cf
Zarar Ahmed <zarar.ahmed@konghamstageek.com>
2019-10-04 16:50:36 +0500

    [ADD] api consumption ready

--- src/scenes/Login/Login.js
+++ src/scenes/Login/Login.js
@@ -1,6 +1,20 @@
 import React, {Component} from 'react';
-
+import axios from "axios";
 export default class Login extends Component{
+    componentDidMount(){
+        axios.get('https://ivol-server.herokuapp.com/api/test')
+            .then(function (response) {
+                // handle success
+                console.log(response);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .finally(function () {
+                // always executed
+            });
+    }
     render(){
         return(
             <div><h1>Login</h1></div>
