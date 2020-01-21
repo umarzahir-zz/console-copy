4cdf2ff33e1d25410468fe2d69d6c17b4aaad607
umarzahir <umar.zahir@konghamstageek.com>
2020-01-21 18:54:04 +0500

    added image upload to opportunity

--- src/scenes/AddOpportunity/index.js
+++ src/scenes/AddOpportunity/index.js
@@ -1,57 +1,65 @@
-import React, {Component}  from "react";
-import  Navigation from "../../components/Navigation/index.js";
+import React, { Component } from "react";
+import Navigation from "../../components/Navigation/index.js";
 import Calendar from 'react-calendar';
 import { connect } from "react-redux";
-import { opportunityData  } from '../../actions/opportunityAction'
+import { opportunityData } from '../../actions/opportunityAction'
 
 
-class AddOpportunity extends Component{
-    constructor(props){
+class AddOpportunity extends Component {
+    constructor(props) {
         super(props)
         this.state = {
             title: "",
-            opportunityLevel:"",
-            peopleRequired:"",
+            opportunityLevel: "",
+            peopleRequired: "",
             date: new Date(),
-            calendar: false
+            calendar: false,
+            imageSrc: require('../../images/user.png'),
+            imageData: null
 
         }
 
     }
-    
-    showCalender= ()=> {
-        this.setState({calendar: true})
+
+    showCalender = () => {
+        this.setState({ calendar: true })
     }
-    hideCalender= (data)=> {
-        this.setState({ date: data , calendar: false})
-    
-        
+    hideCalender = (data) => {
+        this.setState({ date: data, calendar: false })
+
+
     }
     titleChange = (event) => {
         console.log(event.target.value);
-        this.setState({[event.target.name]: event.target.value});
-        
+        this.setState({ [event.target.name]: event.target.value });
+
     }
     handleFormButton = (event) => {
         event.preventDefault()
         this.props.sendFormData(this.state)
     }
     onChange = date => this.setState({ date })
+    handleImage = (event) => {
 
-    render(){
-        console.log(this.state.date)
-        
+        console.log(event.target.files[0])
+        this.setState({ imageData: event.target.files[0], imageSrc: require(event.target.files[0].name) })
 
-       const justDate = this.state.date.toString().split(' ')[0] + " " +  this.state.date.toString().split(' ')[1] + " " + this.state.date.toString().split(' ')[2] + " " +  this.state.date.toString().split(' ')[3]
+    }       
+    render() {
+        console.log(this.state.date)  
             
-        return(
+
+
+        const justDate = this.state.date.toString().split(' ')[0] + " " + this.state.date.toString().split(' ')[1] + " " + this.state.date.toString().split(' ')[2] + " " + this.state.date.toString().split(' ')[3]
+
+        return (
             <div className="wrapper">
                 <div className="container-fluid">
                     <div className="row">
                         <aside>
-                            <Navigation/> 
+                            <Navigation />
                         </aside>
-                        <article>       
+                        <article>
                             <div className="content">
                                 <div className="dashboard">
                                     <div className="page_title">
@@ -59,63 +67,64 @@ class AddOpportunity extends Component{
                                     </div>
 
                                     <div className="center-content">
-                                       
-                                   
-                                            <div className="opportunity-form col-12 p-0">
-                                                <form className="col-6 data-form">
-                                                    <div className="form-group profile-pic d-flex flex-wrap">
-                                                        <label>opportunity image</label>
-                                                        <span className="profilepic-outer">
-                                                            <img src={require('../../images/user.png')} alt=""/>
-                                                        </span>
-                                                        <button className="btn btn-primary gray-button">upload</button>
-                                                    </div>
-                                                    <div className="form-group">
-                                                        <label>Opportunity Name</label>
-                                                        <input className="form-control" 
-                                                        type="text" value = {this.state.title} 
+
+
+                                        <div className="opportunity-form col-12 p-0">
+                                            <form className="col-6 data-form">
+                                                <div className="form-group profile-pic d-flex flex-wrap">
+                                                    <label>opportunity image</label>
+                                                    <span className="profilepic-outer">
+                                                        <input type='file' name='file' onChange={this.handleImage} ></input>
+                                                        <img src={this.state.imageSrc} alt="image" />
+                                                    </span>
+                                                    <button className="btn btn-primary gray-button">upload</button>
+                                                </div>
+                                                <div className="form-group">
+                                                    <label>Opportunity Name</label>
+                                                    <input className="form-control"
+                                                        type="text" value={this.state.title}
                                                         placeholder="Oppotunity name"
                                                         name="title"
-                                                        onChange={this.titleChange}/>
-                                                        
-                                                    </div>
-                                                    <div className="form-group">
-                                                        <label>Opportunity Level</label>
-                                                        <select className="form-control" 
-                                                        name="opportunityLevel"  
-                                                        value={this.state.opportunityLevel} 
-                                                        onChange={this.titleChange}>
-                                                            <option>Beginner</option>
-                                                            <option>Expert</option>
-                                                        </select>
-                                                    </div>
-                                                    <div className="form-group">
-                                                        <label>people required</label>
-                                                        <input className="form-control" 
-                                                        placeholder="People required" 
-                                                        name="peopleRequired" 
-                                                        value={this.state.peopleRequired} 
-                                                        onChange={this.titleChange}/>
-                                                    </div>
-                                                    <div className="form-group">
-                                                        <label>Opportunity Start Date</label>
-                                                        <input className="form-control" 
-                                                        placeholder="dd/mm/yyy" 
-                                                        name="calender" 
-                                                        onClick={this.showCalender}
-                                                        value={justDate} 
-                                                        onChange={this.titleChange}/>
+                                                        onChange={this.titleChange} />
 
-                                                        { this.state.calendar && <Calendar
-                                                            onChange={this.onChange}
-                                                            value={this.state.date}
-                                                            onClickDay={(data)=> this.hideCalender(data)}
-                                                            />}
-                                                    </div>
-                                                    <button onClick={this.handleFormButton} type="submit" className="btn btn-primary gray-button" >Add Opportunity</button>
-                                                </form>
-                                            </div>
-                                       
+                                                </div>
+                                                <div className="form-group">
+                                                    <label>Opportunity Level</label>
+                                                    <select className="form-control"
+                                                        name="opportunityLevel"
+                                                        value={this.state.opportunityLevel}
+                                                        onChange={this.titleChange}>
+                                                        <option>Beginner</option>
+                                                        <option>Expert</option>
+                                                    </select>
+                                                </div>
+                                                <div className="form-group">
+                                                    <label>people required</label>
+                                                    <input className="form-control"
+                                                        placeholder="People required"
+                                                        name="peopleRequired"
+                                                        value={this.state.peopleRequired}
+                                                        onChange={this.titleChange} />
+                                                </div>
+                                                <div className="form-group">
+                                                    <label>Opportunity Start Date</label>
+                                                    <input className="form-control"
+                                                        placeholder="dd/mm/yyy"
+                                                        name="calender"
+                                                        onClick={this.showCalender}
+                                                        value={justDate}
+                                                        onChange={this.titleChange} />
+
+                                                    {this.state.calendar && <Calendar
+                                                        onChange={this.onChange}
+                                                        value={this.state.date}
+                                                        onClickDay={(data) => this.hideCalender(data)}
+                                                    />}
+                                                </div>
+                                                <button onClick={this.handleFormButton} type="submit" className="btn btn-primary gray-button" >Add Opportunity</button>
+                                            </form>
+                                        </div>
+
 
                                     </div>
 
@@ -123,7 +132,7 @@ class AddOpportunity extends Component{
                             </div>
                         </article>
                     </div>
-               </div>
+                </div>
             </div>
         )
     }
@@ -133,4 +142,4 @@ const mapDispatchToProps = (dispatch) => {
         sendFormData: (data) => dispatch(opportunityData(data))
     }
 }
-export default connect(null,mapDispatchToProps)(AddOpportunity)
+export default connect(null, mapDispatchToProps)(AddOpportunity)
