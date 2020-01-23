f38fabbf48c564c29986ff70c4ce19bb3f639d8f
umarzahir <umar.zahir@konghamstageek.com>
2020-01-23 12:06:11 +0500

    get all opportunity and print it in mangeopportunity file

--- src/actions/opportunityAction.js
+++ src/actions/opportunityAction.js
@@ -1,42 +1,42 @@
-import  axios  from "axios";
+import axios from "axios";
 
 import { ADD_OPPORTUNITY } from "./types";
-import {MANAGE_OPPORTUNITY} from './types';
-export const opportunityData = (formData) =>{
+import { MANAGE_OPPORTUNITY } from './types';
+export const opportunityData = (formData) => {
 
-    console.log("this is the data that we are sendig to api", formData)
+    // console.log("this is the data that we are sendig to api", formData)
     return (dispatch, getState) => {
-        axios.post('https://ivol-server.herokuapp.com/api/opportunity/',formData)
-        .then(res => {
-            console.log(res)
-            dispatch({
-                type:ADD_OPPORTUNITY,
-                payload:res.data
+        axios.post('https://ivol-server.herokuapp.com/api/opportunity/', formData)
+            .then(res => {
+
+                dispatch({
+                    type: ADD_OPPORTUNITY,
+                    payload: res.data
+                })
             })
-        }) 
-        .catch(err=>{
-            
-        })
+            .catch(err => {
+
+            })
 
     }
-   
+
 }
 
 export const manageOpportunity = () => {
     return (dispatch) => {
         axios.get('https://ivol-server.herokuapp.com/api/opportunity')
-        .then(res=>{
-            console.log('response of manage oppotunites', res )
-            dispatch({
-                type:MANAGE_OPPORTUNITY,
-                payload:res.data
+            .then(res => {
+                console.log('response of manage oppotunites', res)
+                dispatch({
+                    type: MANAGE_OPPORTUNITY,
+                    payload: res.data
+                })
             })
-        })
-        .catch({
-    
-        })
+            .catch({
+
+            })
     }
-    
+
 }
 
 
