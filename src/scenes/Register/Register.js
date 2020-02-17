eeac35f79a42dae7bb1f9f0d062d6452c8e624d1
umarzahir <umar.zahir@konghamstageek.com>
2020-02-17 18:30:42 +0500

    admin signup done

--- src/scenes/Register/Register.js
+++ src/scenes/Register/Register.js
@@ -1,30 +1,72 @@
-import React, {Component} from 'react';
-import { Link } from "react-router-dom";
-export default class Register extends Component{
-    render(){
-        return(
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { Alert } from 'reactstrap';
+
+// import { Link } from "react-router-dom";
+import { adminSignup } from '../../actions/adminAction'
+class Register extends Component {
+
+
+    state = { email: '', password: '', confirmPassword: '', }
+    handleSignup = (event) => {
+        console.log("signup event".event)
+        this.props.signup(this.state)
+    }
+    handleChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value })
+    }
+    onDismiss = () => {
+        this.setState({ visible: false })
+    }
+    render() {
+
+
+        return (
             <div className="d-flex m-auto form-container text-left flex-wrap">
                 <div className="w-100">
-                <div className="">
-                    <h1>New Admin Account</h1>
-                </div>
-                <form className="col-12 form">
+                    <div className="">
+                        <h1>New Admin Account</h1>
+                    </div>
+                    {/* <form className="col-12 form"> */}
                     <div className="form-group">
                         <label htmlFor="exampleInputEmail1">Email address</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                        <input onChange={this.handleChange} name="email" type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
                     </div>
                     <div className="form-group">
                         <label htmlFor="password1">Password</label>
-                        <input type="password" className="form-control" id="password1" />
+                        <input onChange={this.handleChange} name="password" type="password" className="form-control" id="password1" />
                         <label htmlFor="password2">Repeat Password</label>
-                        <input type="password" className="form-control" id="password2" />
+                        <input onChange={this.handleChange} name="confirmPassword" type="password" className="form-control" id="password2" />
                     </div>
                     <div className="form-footer w-100 d-flex">
-                    <Link type="submit" to='/login' className="btn btn-primary form-button">Submit</Link>
+
+                        < Alert color="info" isOpen={this.props.visible} toggle={this.onDismiss}>
+                            {this.props.signupMessage}
+                        </Alert>
+
+
+                        <button onClick={this.handleSignup} >
+                            {this.props.signupState ? "Loading" : "SignUp"}
+                        </button>
+                        {/* <Link type="submit" to='/login' className="btn btn-primary form-button">Submit</Link> */}
                     </div>
-                </form>    
+                    {/* </form> */}
                 </div>
-            </div>
+            </div >
         )
     }
 }
+const mapStateToProps = (state) => {
+    return {
+        signupState: state.admin.adminSignupStaus,
+        signupMessage: state.admin.adminSignupMessage,
+        visible: state.admin.message
+    }
+}
+const mapDispatchToProps = (dispatch) => {
+    return {
+        signup: (data) => { dispatch(adminSignup(data)) }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Register)
