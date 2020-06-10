c78bcb171e92d85a374f85f6237c9a79fbdc42bd
Ali Zahid <alizahid@Alis-iMac.local>
2020-06-10 16:51:02 +0500

    Added form checks and changed function methods

--- src/scenes/Register/Register.js
+++ src/scenes/Register/Register.js
@@ -10,79 +10,86 @@ class Register extends Component {
         this.onChange = this.onChange.bind(this);
 
         this.state = { 
+            name: '',
             email: '',
             password: '', 
             confirmPassword: '', 
             imageUrl: null,
             imageData: null,
+            error: ''
         }
     }
 
     
     handleSignup = (event) => {
-        console.log("signup event".event)
-        this.props.signup(this.state)
+        console.log("signup event", event)
+        const { password, confirmPassword} = this.state;
+        if(password !== confirmPassword)
+        {
+            alert("The Passwords Dont Match. Please Try again!")
+            event.preventDefault();
+        }
+        else {
+            this.props.signup(this.state)
+        }
+        
     }
     handleChange = (event) => {
         this.setState({ [event.target.name]: event.target.value })
+        console.log(event.target.value)
     }
     onDismiss = () => {
         this.setState({ visible: false })
     }
-    onChange(e) {
+    onChange = (e) => {
         let files = e.target.files;
         console.log(files);
-        // console.log("image upload", e.target.files[0])
-        // const tempUrl = URL.createObjectURL(e.target.files[0])
-        // this.setState({ imageData: e.target, imageUrl: tempUrl })
-        // console.log('Second log ', e.target.files[0]);
+        console.log("Image data", e.target.files[0])
+        const tempUrl = URL.createObjectURL(e.target.files[0])
+        this.setState({ imageData: e.target, imageUrl: tempUrl })
+        console.log('Second log ', e.target.files[0]);
     }
     render() {
-        
-
-
         return (
             <div className="d-flex m-auto form-container text-left flex-wrap">
                 <div className="w-100">
                     <div className="">
                         <h1>New Agency Account</h1>
                     </div>
-                    <form className="col-12 form">
-                    <div className="form-group">
-                        <label htmlFor="exampleInputEmail1">Name</label>
-                        <input onChange={this.handleChange} name="name" type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="exampleInputEmail1">Email address</label>
-                        <input onChange={this.handleChange} name="email" type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password1">Password</label>
-                        <input onChange={this.handleChange} name="password" type="password" className="form-control" id="password1" />
-                        <label htmlFor="password2">Repeat Password</label>
-                        <input onChange={this.handleChange} name="confirmPassword" type="password" className="form-control" id="password2" />
-                        <label>Upload Image</label>
+                    <form onSubmit={this.handleSubmit} className="col-12 form">
+                        <div className="form-group">
+                            <label htmlFor="name">Name</label>
+                            <input onChange={this.handleChange} name="name" value={this.state.name} type="text" className="form-control" id="name" aria-describedby="emailHelp" required/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="email">Email address</label>
+                            <input onChange={this.handleChange} name="email" value={this.state.email} type="email" className="form-control" id="email" aria-describedby="emailHelp" required/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password1">Password</label>
+                            <input onChange={this.handleChange} name="password" type="password" value={this.state.password} className="form-control" id="password1" required/>
+                            <label htmlFor="password2">Repeat Password</label>
+                            <input onChange={this.handleChange} name="confirmPassword" type="password" value={this.state.confirmPassword} className="form-control" id="password2" required/>
+                            <label>Upload Image</label>
 
-                        {/* <input type="file" name='image' accept="image/*" onChange={this.handleImage} ></input> */}
+                            <input type="file" name="image" accept="image/*" onChange={this.onChange} required/>
+                            {this.state.imageUrl ? <img style={{width: "70%", paddingTop: 10}} src={this.state.imageUrl} alt="" /> : <img src={this.state.imageSrc} alt="" />}
+                        </div>                        
+                        <div className="form-footer w-100 d-flex">
 
-                        <input type="file" name="image" accept="image/*" onChange={(e)=>this.onChange(e)} />
-                        {this.state.imageUrl ? <img style={{width: "70%", paddingTop: 10}} src={this.state.imageUrl} alt="" /> : <img src={this.state.imageSrc} alt="" />}
-                    </div>
-                    <div className="form-footer w-100 d-flex">
-
-                        < Alert color="info" isOpen={this.props.visible} toggle={this.onDismiss}>
-                            {this.props.signupMessage}
-                        </Alert>
+                            < Alert color="info" isOpen={this.props.visible} toggle={this.onDismiss}>
+                                {this.props.signupMessage}
+                            </Alert>
 
 
-                        <button onClick={this.handleSignup} >
-                            {this.props.signupState ? "Loading" : "SignUp"}
-                        </button>
-                        {/* <Link type="submit" to='/login' className="btn btn-primary form-button">Submit</Link> */}
-                    </div>
+                            <button type="submit" onClick={this.handleSignup} >
+                                {this.props.signupState ? "Loading" : "SignUp"}
+                            </button>
+                            {/* <Link type="submit" to='/login' className="btn btn-primary form-button">Submit</Link> */}
+                        </div>
                     </form>
                 </div>
-            </div >
+            </div>
         )
     }
 }
