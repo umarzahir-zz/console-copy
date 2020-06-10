21537e16f27c98d2e8865038ace80bc4b9203712
umarzahir <umar.zahir@konghamstageek.com>
2020-06-10 22:49:06 +0500

    send data to backend

--- src/scenes/Register/Register.js
+++ src/scenes/Register/Register.js
@@ -9,17 +9,18 @@ class Register extends Component {
         super(props);
         this.onChange = this.onChange.bind(this);
 
-        this.state = { 
+        this.state = {
             email: '',
-            password: '', 
-            confirmPassword: '', 
+            password: '',
+            confirmPassword: '',
             imageUrl: null,
-            imageData: null,
+            selectedFile: null
         }
     }
 
-    
+
     handleSignup = (event) => {
+        event.preventDefault()
         console.log("signup event".event)
         this.props.signup(this.state)
     }
@@ -30,17 +31,11 @@ class Register extends Component {
         this.setState({ visible: false })
     }
     onChange(e) {
-        let files = e.target.files;
-        console.log(files);
-        // console.log("image upload", e.target.files[0])
-        // const tempUrl = URL.createObjectURL(e.target.files[0])
-        // this.setState({ imageData: e.target, imageUrl: tempUrl })
-        // console.log('Second log ', e.target.files[0]);
+        const tempUrl = URL.createObjectURL(e.target.files[0])
+        this.setState({ selectedFile: e.target.files[0], imageUrl: tempUrl })
+        console.log("image upload", e.target.files[0])
     }
     render() {
-        
-
-
         return (
             <div className="d-flex m-auto form-container text-left flex-wrap">
                 <div className="w-100">
@@ -48,38 +43,38 @@ class Register extends Component {
                         <h1>New Agency Account</h1>
                     </div>
                     <form className="col-12 form">
-                    <div className="form-group">
-                        <label htmlFor="exampleInputEmail1">Name</label>
-                        <input onChange={this.handleChange} name="name" type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="exampleInputEmail1">Email address</label>
-                        <input onChange={this.handleChange} name="email" type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password1">Password</label>
-                        <input onChange={this.handleChange} name="password" type="password" className="form-control" id="password1" />
-                        <label htmlFor="password2">Repeat Password</label>
-                        <input onChange={this.handleChange} name="confirmPassword" type="password" className="form-control" id="password2" />
-                        <label>Upload Image</label>
+                        <div className="form-group">
+                            <label htmlFor="exampleInputEmail1">Name</label>
+                            <input onChange={this.handleChange} name="name" type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="exampleInputEmail1">Email address</label>
+                            <input onChange={this.handleChange} name="email" type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password1">Password</label>
+                            <input onChange={this.handleChange} name="password" type="password" className="form-control" id="password1" />
+                            <label htmlFor="password2">Repeat Password</label>
+                            <input onChange={this.handleChange} name="confirmPassword" type="password" className="form-control" id="password2" />
+                            <label>Upload Image</label>
 
-                        {/* <input type="file" name='image' accept="image/*" onChange={this.handleImage} ></input> */}
+                            {/* <input type="file" name='image' accept="image/*" onChange={this.handleImage} ></input> */}
 
-                        <input type="file" name="image" accept="image/*" onChange={(e)=>this.onChange(e)} />
-                        {this.state.imageUrl ? <img style={{width: "70%", paddingTop: 10}} src={this.state.imageUrl} alt="" /> : <img src={this.state.imageSrc} alt="" />}
-                    </div>
-                    <div className="form-footer w-100 d-flex">
+                            <input type="file" name="fileData" accept="image/*" onChange={(e) => this.onChange(e)} />
+                            {this.state.imageUrl ? <img style={{ width: "70%", paddingTop: 10 }} src={this.state.imageUrl} alt="" /> : <img src={this.state.imageSrc} alt="" />}
+                        </div>
+                        <div className="form-footer w-100 d-flex">
 
-                        < Alert color="info" isOpen={this.props.visible} toggle={this.onDismiss}>
-                            {this.props.signupMessage}
-                        </Alert>
+                            < Alert color="info" isOpen={this.props.visible} toggle={this.onDismiss}>
+                                {this.props.signupMessage}
+                            </Alert>
 
 
-                        <button onClick={this.handleSignup} >
-                            {this.props.signupState ? "Loading" : "SignUp"}
-                        </button>
-                        {/* <Link type="submit" to='/login' className="btn btn-primary form-button">Submit</Link> */}
-                    </div>
+                            <button onClick={this.handleSignup} >
+                                {this.props.signupState ? "Loading" : "SignUp"}
+                            </button>
+                            {/* <Link type="submit" to='/login' className="btn btn-primary form-button">Submit</Link> */}
+                        </div>
                     </form>
                 </div>
             </div >
