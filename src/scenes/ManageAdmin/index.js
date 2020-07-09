e81153a947a7743bfc3223bc31ba2f5400dbf8b4
umarzahir <umar.zahir@konghamstageek.com>
2020-07-09 22:51:21 +0500

    admin credeintial  chnage ui

--- src/scenes/ManageAdmin/index.js
+++ src/scenes/ManageAdmin/index.js
@@ -1,19 +1,25 @@
 import React, { Component } from "react";
 import Navigation from "../../components/Navigation/index.js";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
-import { ListGroup, Modal, Button } from "react-bootstrap";
+import { faTrash,  faUserEdit } from '@fortawesome/free-solid-svg-icons'
+import { ListGroup, Form, Modal, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export default class ManageAdmin extends Component {
-  state = { show: false , adminName: null};
+  state = { show: false , show2: false, adminName: null};
   handleClose = () => {
     this.setState({ show: false });
   };
+  handleClose2 = () => {
+    this.setState({ show2: false });
+  };
   handleShow = (admin) => {
     console.log("handle show clicked");
-    
     this.setState({ show: true, adminName: admin });
+  };
+  handleShow2 = () => {
+    console.log("handle show clicked");
+    this.setState({ show2: true, });
   };
   handleConfirm = () => {
     
@@ -35,7 +41,36 @@ export default class ManageAdmin extends Component {
                   <div className="page_title">
                     <h1>manage admin</h1>
                   </div>
-                  {/* model */}
+                  {/* change credential model */}
+                  <Modal show={this.state.show2} onHide={this.handleClose2}>
+                    <Modal.Header closeButton>
+                      <Modal.Title>Change credential</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                    <Form>
+                      <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                      </Form.Group>
+                      <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                      </Form.Group>
+                      <Button variant="primary" type="submit">
+                        Submit
+                      </Button>
+                    </Form>
+                    </Modal.Body>
+                    <Modal.Footer>
+                      <Button variant="secondary" onClick={this.handleClose2}>
+                        Close
+                      </Button>
+                      <Button variant="success" onClick={this.handleClose2}>
+                        Update
+                      </Button>
+                    </Modal.Footer>
+                  </Modal>
+                  {/*  model */}
                   <Modal show={this.state.show} onHide={this.handleClose}>
                     <Modal.Header closeButton>
                       <Modal.Title>{this.state.adminName}</Modal.Title>
@@ -60,11 +95,18 @@ export default class ManageAdmin extends Component {
                       <p>{admin}</p>
                       </ListGroup.Item>
                       </div>
-                      <div className="col-2 pt-2 "  >
-                      <button onClick={this.handleConfirm} className="btn btn-danger">
-                      <FontAwesomeIcon icon={faTrash}/>
+                      <div className="col-1 pt-2 "  >
+                      <button  onClick={this.handleShow2} className="btn btn-primary">
+                      <FontAwesomeIcon icon={faUserEdit}/>
                         </button>
                       </div>
+                      <div className="col-1 pt-2 "  >
+                      <button  onClick={this.handleConfirm} className="btn btn-danger">
+                      <FontAwesomeIcon icon={faTrash}/>
+                      {/* <FontAwesomeIcon icon={faUserEdit}/> */}
+                        </button>
+                      </div>
+                      
                     </div>
                 </ListGroup>)
                   })}
